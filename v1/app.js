0d4a5a09264f83635df06253267536a8177b3a70
phetgary <56030176+phetgary@users.noreply.github.com>
2020-05-28 00:26:54 +0800

    add POST n Form

--- v1/app.js
+++ v1/app.js
@@ -1,20 +1,35 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+var campgrounds = [
+    {name: "Salmon Creek", image: "https://pixabay.com/get/57e1d14a4e52ae14f1dc84609620367d1c3ed9e04e507440752a7cd0954bc2_340.jpg"},
+    {name: "Grantie Hill", image: "https://pixabay.com/get/52e8d4444255ae14f1dc84609620367d1c3ed9e04e507440752a7cd0954bc2_340.jpg"},
+    {name: "Mountain Goat's Rest", image: "https://pixabay.com/get/57e8d0424a5bae14f1dc84609620367d1c3ed9e04e507440752a73d3924cc0_340.jpg"}
+]
 
 app.get("/", function(req, res){
     res.render("landing");
 })
 
 app.get("/campgrounds", function(req, res){
-    var campgrounds = [
-        {name: "Salmon Creek", image: "https://pixabay.com/get/57e1d14a4e52ae14f1dc84609620367d1c3ed9e04e507440752a7cd0954bc2_340.jpg"},
-        {name: "Grantie Hill", image: "https://pixabay.com/get/52e8d4444255ae14f1dc84609620367d1c3ed9e04e507440752a7cd0954bc2_340.jpg"},
-        {name: "Mountain Goat's Rest", image: "https://pixabay.com/get/57e8d0424a5bae14f1dc84609620367d1c3ed9e04e507440752a73d3924cc0_340.jpg"}
-    ]
     res.render("campgrounds", {campgrounds: campgrounds});
 })
+
+app.post("/campgrounds", function(req, res){
+    var name = req.body.name; 
+    var image = req.body.image;
+    var newCampground = {name: name, image: image};
+    campgrounds.push(newCampground);
+    res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", function(req, res){
+    res.render("new.ejs");
+});
 
 app.listen(3000, function(){
     console.log("The YelpCamp server has started!!!")
