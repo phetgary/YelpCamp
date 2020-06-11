a1a7bd73e350ab5d32c8f2536b64d214274952f4
phetgary <56030176+phetgary@users.noreply.github.com>
2020-06-11 17:50:46 +0800

    Add comment model

--- v3/app.js
+++ v3/app.js
@@ -1,18 +1,16 @@
 var express     = require("express"),
     app         = express(),
     bodyParser  = require("body-parser"),
-    mongoose    = require("mongoose")
-mongoose.connect("mongodb://localhost/yelp_camp")
+    mongoose    = require("mongoose"),
+    Campground  = require("./models/campground")
+    seedDB      = require("./seeds");
 
+
+
+mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
-
-var campgroundSchema = new mongoose.Schema({
-    name: String,
-    image: String,
-    description: String
-});
-var Campground = mongoose.model("Campground", campgroundSchema);
+seedDB();
 
 // Campground.create(    
 //     {name: "Grantie Hill",
@@ -66,10 +64,11 @@ app.get("/campgrounds/new", function(req, res){
 });
 
 app.get("/campgrounds/:id", function(req, res){
-    Campground.findById(req.params.id, function(err, foundCampground){
+    Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
         if(err){
             console.log(err);
         } else {
+            console.log(foundCampground);
                res.render("show", {campground:foundCampground});
         }
     })
